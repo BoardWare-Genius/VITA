37bf14625aef47a2f45e3978d65e6032b914b6e8
Ivan087 <wujiatai087@gmail.com>
2025-04-07 01:45:58 +0000

    feat: add TOUR-YN benchmark

--- VLMEvalKit/vlmeval/dataset/__init__.py
+++ VLMEvalKit/vlmeval/dataset/__init__.py
@@ -8,7 +8,9 @@ from .image_mt import MMDUDataset
 from .image_vqa import (
     ImageVQADataset, MathVision, OCRBench, MathVista, LLaVABench, MMVet, MTVQADataset, TableVQABench, CustomVQADataset
 )
+
 from .image_mo_mcq import TourMCQ
+from .image_mo_yorn import TourYN
 
 from .vcr import VCRDataset
 from .mmlongbench import MMLongBench
@@ -113,7 +115,7 @@ class ConcatDataset(ImageBaseDataset):
 IMAGE_DATASET = [
     ImageCaptionDataset, ImageYORNDataset, ImageMCQDataset, ImageVQADataset, MathVision,
     MMMUDataset, OCRBench, MathVista, LLaVABench, MMVet, MTVQADataset, TableVQABench,
-    MMLongBench, VCRDataset, MMDUDataset, DUDE, SlideVQA, MUIRDataset, GMAIMMBenchDataset, MMERealWorld,TourMCQ
+    MMLongBench, VCRDataset, MMDUDataset, DUDE, SlideVQA, MUIRDataset, GMAIMMBenchDataset, MMERealWorld,TourMCQ, TourYN
 ]
 
 VIDEO_DATASET = [
